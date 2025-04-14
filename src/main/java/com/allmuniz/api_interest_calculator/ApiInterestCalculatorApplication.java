ab42a8349e938b774afae82379bb16ed69bb2209
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-04-14 17:54:33 -0300

    feat: Adding swagger config

--- src/main/java/com/allmuniz/api_interest_calculator/ApiInterestCalculatorApplication.java
+++ src/main/java/com/allmuniz/api_interest_calculator/ApiInterestCalculatorApplication.java
@@ -1,9 +1,18 @@
 package com.allmuniz.api_interest_calculator;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Interest Calculator",
+				description = "Interest calculation service",
+				version = "1"
+		)
+)
 public class ApiInterestCalculatorApplication {
 
 	public static void main(String[] args) {
