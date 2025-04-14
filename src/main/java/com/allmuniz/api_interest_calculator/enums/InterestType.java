eb7ff163a418b44fac14736312cc11e4effa686b
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-04-14 17:55:36 -0300

    refactor: Updating in the enum

--- src/main/java/com/allmuniz/api_interest_calculator/enums/InterestType.java
+++ src/main/java/com/allmuniz/api_interest_calculator/enums/InterestType.java
@@ -1,6 +1,27 @@
 package com.allmuniz.api_interest_calculator.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum InterestType {
-    SIMPLE,
-    COMPOUND
+    SIMPLE(1, "Simple"),
+    COMPOUND(2, "Compound");
+
+    private final int id;
+    private final String description;
+
+    InterestType(int id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public static InterestType fromId(int id) {
+        for (InterestType type : values()) {
+            if (type.getId() == id) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Invalid InterestType id: " + id);
+    }
 }
+
