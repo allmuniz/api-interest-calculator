70d7a893e980c7c99822799ceb116916941246ef
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-04-14 16:35:20 -0300

    first commit

---
+++ src/main/java/com/allmuniz/api_interest_calculator/ApiInterestCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.allmuniz.api_interest_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInterestCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInterestCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allmuniz/api_interest_calculator/ApiInterestCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allmuniz.api_interest_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInterestCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
