2528cd3cda8f1bf95a8af329f87addc8b9187ab7
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-04-14 17:56:03 -0300

    refactor: Updating in the request dto

--- src/main/java/com/allmuniz/api_interest_calculator/dtos/InterestRequestDTO.java
+++ src/main/java/com/allmuniz/api_interest_calculator/dtos/InterestRequestDTO.java
@@ -1,11 +1,10 @@
 package com.allmuniz.api_interest_calculator.dtos;
 
-import com.allmuniz.api_interest_calculator.enums.InterestType;
-
 public record InterestRequestDTO(
         Double capital,
         Double interestRate,
         Integer time,
-        InterestType type
+        Integer typeTime,
+        Integer type
 ) {
 }
